60533023328198a78d6d73f02af2aec610389ce5
Gaurav <gauravjharbade2006@gmail.com>
2025-02-01 16:17:23 +0530

    company assisment

---
+++ p1/api/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import *
+from rest_framework.response import Response
+import pandas as pd
+import io
+from django.http import FileResponse
+from .models import *
+from django.core.files.base import ContentFile
+
+
+# Create your views here.
+SALES_INCREASE = {
+    "A": 0.10,
+    "B": 0.075,
+    "C": 0.08,
+    "D": 0.12
+}
+
+
+
+@api_view(["POST"])
+def fileUploader(request):
+   
+
+    file1 = request.FILES['file1']
+    file2 = request.FILES['file2']
+
+    try:
+        fl1 = pd.read_excel(io.BytesIO(file1.read()))
+        fl2 = pd.read_excel(io.BytesIO(file2.read()))
+    except Exception as e:
+        return Response({"error": f"Error reading files: {str(e)}"}, status=400)
+
+    allfile = pd.merge(fl1, fl2, on='SKUs')
+    allfile["Sales Forecast"] = allfile["Last month sales"] * allfile["SKUs"].map(SALES_INCREASE)
+    allfile["Purchase Order (PO)"] = allfile["Sales Forecast"] * allfile["Price"]
+
+    output_file = io.BytesIO()
+    allfile.to_excel(output_file, index=False)
+
+    processed_file = Uploadfile()
+    processed_file.file.save("processed_sales.xlsx", ContentFile(output_file.read()))
+    processed_file.save()
+
+    serializer = UploadFileserializear(processed_file)
+
+    response = FileResponse(output_file, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="processed_sales.xlsx"'
+
+    return Response(serializer.data, status=201)  
+
+
